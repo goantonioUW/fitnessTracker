e1e9fe7aac18d7fd71d8dd3046fd88d718b2450e
Antonio Gonzalez <goantonio31@outlook.com>
2021-02-19 14:25:48 -0800

    initiall commit adding the serverJs and folders

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const logger = require("morgan");
+
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
+
+const app = express();
+
+app.use(logger("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+
+// db.BigWorkout.create({ name: "" })
+//   .then( => {})
+//   .catch(({message}) => {});
+
+// app.post("/", ({body}, res) => {});
+
+// app.get("/", (req, res) => {});
