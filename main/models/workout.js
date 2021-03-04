e58a3c46491334dac1872efe9fb7dd5648e45bc0
Antonio Gonzalez <goantonio31@outlook.com>
2021-03-03 22:04:16 -0800

    updated file structure added code to get the app working

--- main/models/workout.js
+++ main/models/workout.js
@@ -1,54 +1,58 @@
 const mongoose = require("mongoose");
-
+const opts = { toJSON: { virtuals: true } };
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema(
- {
-     day: {
-        type: Date,
-        default: () => new Date()
-    },
- },
- {
-exercise: [
+const workoutSchema = new Schema({
+  day:{
+    type: Date,
+    default: new Date().setDate( new Date().getDate())
+  },
+
+  exercises: [
     {
-        type:{
-            type: String,
-            trim: true,
-            required: "Enter an exercise type"
-        },
-    name: {
+      type: {
+          type: String,
+          trim: true,
+          required: "Select a workout type."
+      },
+      name: {
         type: String,
         trim: true,
-        required: "Enter an exercise type"
+        required: "Please enter a workout name."
+      },
+      
+      duration:{
+          type: Number,
+          required: "Please enter workout duration"
+        },
+      
+      weight: {
+        type: Number
+      },
+      
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      distance: {
+        type: Number
+      }
+  }],
+  
+  
+}, opts );
 
-    },
-    weight: {
-        type: number
-    },
-    sets: {
-        type: number
-    },
-    reps: {
-        type: number
-    }
-},
-]
- },
-    {
-        toJSON: {virtuals: true}
-    });
-    
-    workoutSchema.virtual("totalDuration").get(function(){
-    
-        return this.exercise.reduce( (acc, exercise) => {
-    
-            return acc + exercise.duration;
-    
-        }, 0 );
- });
+workoutSchema.virtual("totalDuration").get(function(){
+  let totalDuration = 0
+  this.exercises.forEach(exercise =>{
+    totalDuration += exercise.duration
+  });
+  return totalDuration
+})
 
- 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSchema);
+
 
 module.exports = Workout;
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static("public"));
+
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+mongoose.connect(MONGODB_URI,{  
+    useNewUrlParser:true,
+    useFindAndModify:false
+})
+
+// routes
+app.use(require("./main/routes/apiRoutes"));
+app.use(require("./main/routes/htmlRoutes"));
+
+app.listen(PORT, ()=>{
+  console.log(`App is running on port: ${PORT}`)
+});
